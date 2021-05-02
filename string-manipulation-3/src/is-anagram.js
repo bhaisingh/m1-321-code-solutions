3fb2771b53e943e62245fc4a62688b200436c928
Rakesh Singh <bhaisingh@gmail.com>
2021-05-01 23:15:50 -0700

    Completed string-manipulation-3.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,21 +1,57 @@
 /* exported isAnagram */
+// @ts-check
 function isAnagram(firstString, secondString) {
   var tempFirstString = '';
-  // var tempSecondString = '';
+  var outFirstString = '';
+  var tempSecondString = '';
+  var outSecondString = '';
+
   for (var i = 0; i < firstString.length; i++) {
     if (firstString[i] !== ' ') {
-      if (tempFirstString.length === 0) {
-        tempFirstString += firstString[i];
+      if (outFirstString.length === 0) {
+        outFirstString += firstString[i];
       } else {
-        for (var j = 0; j < tempFirstString.length; j++) {
-          if (firstString[i] <= tempFirstString[j]) {
-            tempFirstString = firstString[i] + tempFirstString;
+        tempFirstString = '';
+        for (var j = 0; j < outFirstString.length; j++) {
+          if (firstString[i] <= outFirstString[j]) {
+            tempFirstString = tempFirstString + firstString[i] + outFirstString.substring(j, outFirstString.length);
+            outFirstString = tempFirstString;
             break;
-          } else if (j === tempFirstString.length - 1) {
-            tempFirstString = tempFirstString + firstString[i];
+          } else if (j === outFirstString.length - 1) {
+            outFirstString = outFirstString + firstString[i];
+            break;
+          } else {
+            tempFirstString += outFirstString[j];
           }
         }
       }
     }
   }
+
+  for (i = 0; i < secondString.length; i++) {
+    if (secondString[i] !== ' ') {
+      if (outSecondString.length === 0) {
+        outSecondString += secondString[i];
+      } else {
+        tempSecondString = '';
+        for (j = 0; j < outSecondString.length; j++) {
+          if (secondString[i] <= outSecondString[j]) {
+            tempSecondString = tempSecondString + secondString[i] + outSecondString.substring(j, outSecondString.length);
+            outSecondString = tempSecondString;
+            break;
+          } else if (j === outSecondString.length - 1) {
+            outSecondString = outSecondString + secondString[i];
+            break;
+          } else {
+            tempSecondString += outSecondString[j];
+          }
+        }
+      }
+    }
+  }
+
+  if (outFirstString === outSecondString) {
+    return true;
+  }
+  return false;
 }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,60 @@
 /* exported titleCase */
+function titleCase(title) {
+  var outString = '';
+  var stringArray = title.split(' ');
+
+  for (var i = 0; i < stringArray.length; i++) {
+    if ((stringArray[i].length > 3) ||
+        (stringArray[i].toLowerCase() === 'web')) {
+
+      if (stringArray[i][stringArray[i].length - 1] === ':') {
+        if (stringArray[i].slice(0, -1).toLowerCase() === 'javascript') {
+          outString = outString + 'JavaScript:';
+          if (i !== stringArray.length - 1) {
+            outString += ' ';
+          }
+          continue;
+        }
+      }
+
+      if (stringArray[i].toLowerCase() === 'javascript') {
+        outString = outString + 'JavaScript';
+        if (i !== stringArray.length - 1) {
+          outString += ' ';
+        }
+        continue;
+      }
+
+      var tempIndex = stringArray[i].indexOf('-');
+      if (tempIndex > -1) {
+        outString = outString + stringArray[i][0].toUpperCase() + stringArray[i].substring(1, tempIndex + 1) + stringArray[i][tempIndex + 1].toUpperCase() + stringArray[i].substring(tempIndex + 2, stringArray[i].length);
+        if (i !== stringArray.length - 1) {
+          outString += ' ';
+        }
+      } else {
+        outString = outString + stringArray[i][0].toUpperCase() + stringArray[i].substring(1, stringArray[i].length);
+        if (i !== stringArray.length - 1) {
+          outString += ' ';
+        }
+      }
+
+    } else if (stringArray[i].toLowerCase() === 'api') {
+      outString = outString + 'API';
+      if (i !== stringArray.length - 1) {
+        outString += ' ';
+      }
+      continue;
+    } else if (stringArray[i - 1][stringArray[i - 1].length - 1] === ':') {
+      outString = outString + stringArray[i][0].toUpperCase() + stringArray[i].substring(1, stringArray[i].length);
+      if (i !== stringArray.length - 1) {
+        outString += ' ';
+      }
+    } else {
+      outString = outString + stringArray[i];
+      if (i !== stringArray.length - 1) {
+        outString += ' ';
+      }
+    }
+  }
+  return outString;
+}
